0d42d05a0d2a44f933de34bf0de6b760367bbb6f
Barbara Diaz <barbarad91@gmail.com>
2021-03-31 17:00:42 +0200

    Update cors config to include vercel in allowList

--- server/src/config/cors.config.ts
+++ server/src/config/cors.config.ts
@@ -2,10 +2,12 @@ import cors, { CorsOptions } from 'cors'
 import { Express } from 'express'
 
 const corsOptions: () => CorsOptions = () => ({
-  origin: (origin, cb) => {
+  origin: (origin: string, cb) => {
+    const vercelRegex = /https:\/\/wommunity.*\.vercel\.app/
+
     const allowList = [process.env.DOMAIN]
 
-    const originIsAllowed = allowList.includes(origin)
+    const originIsAllowed = allowList.includes(origin) || vercelRegex.test(origin)
 
     cb(null, originIsAllowed)
   },
